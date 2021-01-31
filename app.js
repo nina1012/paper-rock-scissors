cb4fbc475bde4efbf27dd66843c312e780aee145
nina1012 <labferment10@gmail.com>
2021-01-31 16:50:52 +0100

    adding deleted commited changes

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
-function playGame() {
-  const options = ['rock', 'paper', 'scissors'];
-  const game = document.querySelector('.game');
-
-  game.addEventListener('click', e => {
-    e.stopImmediatePropagation();
-  });
-
-  function computerPlay() {
-    const index = Math.floor(Math.random() * options.length);
-    return options[index];
-  }
-
+const game = () => {
+  // modal functionality
   function modalFuncs() {
     const rulesBtn = document.querySelector('.rules');
-    const close = document.querySelector('.close');
+    const close = document.querySelector('.modal__btn');
     const modal = document.querySelector('.modal');
+    const content = document.querySelector('.container');
 
     rulesBtn.addEventListener('click', e => {
       modal.classList.add('show');
+      content.classList.add('blurred');
     });
 
     close.addEventListener('click', e => {
+      e.stopImmediatePropagation();
       modal.classList.remove('show');
+      content.classList.remove('blurred');
     });
   }
 
-  // allowing to open and close modal
+  function playGame() {}
+
   modalFuncs();
-}
-playGame();
+};
+
+game();
+
+// const game = document.querySelector('.game');
+// const result = document.querySelector('.result');
+// const message = document.querySelector('.message');
+
+// game.addEventListener('click', e => {
+//   game.classList.add('hide-game');
+//   setTimeout(e => (game.style.display = 'none'), 1000);
+//   setTimeout(e => (result.style.display = 'flex'), 100);
+//   result.classList.add('show-result');
+
+//   setTimeout(e => message.classList.add('visible'), 2000);
+// });
+
+// document.addEventListener('DOMContentLoaded', e => {
+//   const options = document.querySelectorAll('.option');
+//   options.forEach(option => {
+//     option.classList.add('rotate-options');
+//   });
+// });
